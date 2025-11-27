a3c4f4fb071dc79ad91139e108c5a6e1a9a82546
dusixx <113414935+dusixx@users.noreply.github.com>
2025-11-27 02:58:00 +0200

    chore: add types

---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type User = {
+  id: string; // uuid v4
+  login: string;
+  password: string;
+  version: number; // integer number, increments on update
+  createdAt: number; // timestamp of creation
+  updatedAt: number; // timestamp of last update
+};
+
+export type CreateUserDto = {
+  login: string;
+  password: string;
+};
