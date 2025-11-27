48599100cf4db64a1adccda5cd96cbec48977803
dusixx <113414935+dusixx@users.noreply.github.com>
2025-11-27 07:01:23 +0200

    chore: add common

---
+++ src/common/constants.ts
@@ -0,0 +1,11 @@
+export const HttpStatusCode = {
+  OK: 200,
+  Created: 201,
+  NoContent: 204,
+  BadRequest: 400,
+  NotFound: 404,
+  MethodNotAllowed: 405,
+  Conflict: 409,
+  InternalServerError: 500,
+  BadGateway: 502,
+} as const;
