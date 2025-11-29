a64b22cafde1dd2348a9541d550b84ae28e8f8a0
dusixx <113414935+dusixx@users.noreply.github.com>
2025-11-29 13:23:37 +0200

    refactor: minor changes

--- src/common/utils/style.ts
+++ src/common/utils/style.ts
@@ -1,14 +1,16 @@
 import { styleText } from 'node:util';
 
+type StyleFunc = (...args: unknown[]) => string;
+
 export const style = (mod: Parameters<typeof styleText>[0], ...args: unknown[]): string => {
   return styleText(mod, args.join(' '));
 };
-export const cyan = (...args: unknown[]): string => style('cyan', ...args);
-export const red = (...args: unknown[]): string => style('red', ...args);
-export const yellow = (...args: unknown[]): string => style('yellow', ...args);
-export const green = (...args: unknown[]): string => style('green', ...args);
-export const gray = (...args: unknown[]): string => style('gray', ...args);
-export const magenta = (...args: unknown[]): string => style('magenta', ...args);
+export const cyan: StyleFunc = (...args) => style('cyan', ...args);
+export const red: StyleFunc = (...args) => style('red', ...args);
+export const yellow: StyleFunc = (...args) => style('yellow', ...args);
+export const green: StyleFunc = (...args) => style('green', ...args);
+export const gray: StyleFunc = (...args) => style('gray', ...args);
+export const magenta: StyleFunc = (...args) => style('magenta', ...args);
 
 export const stylizeHttpStatus = (result: number): string => {
   if (result < 200) {
