f2a5fb1afcd7182c8459ee3eb3b75ec608139527
dusixx <113414935+dusixx@users.noreply.github.com>
2025-11-29 12:37:15 +0200

    feat: update common utils

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,9 +2,7 @@ export const UUID_VER = 4;
 export const DEF_PORT = 4000;
 
 export const ErrorMessage = {
-  InvalidOldPassword: 'old password is invalid',
   InvalidUUID: 'invalid UUID',
   InvalidRequestBody: 'request body does not contain required fields',
   NotFound: (s: TemplateStringsArray | string) => `${s[0]} not found`,
-  AlreadyExists: (s: TemplateStringsArray | string) => `${s[0]} already exists`,
 } as const;
