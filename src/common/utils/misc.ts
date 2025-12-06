f5aec15b252bd267f31a10f3b7d56ad9258b60d8
dusixx <113414935+dusixx@users.noreply.github.com>
2025-12-06 20:52:04 +0200

    fix: update import path

--- src/common/utils/misc.ts
+++ src/common/utils/misc.ts
@@ -1,5 +1,5 @@
 import { ParseUUIDPipe } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Prisma } from '../../prisma/generated/client/client';
 import { UUID_VER } from '../constants';
 import { red } from './style';
 
