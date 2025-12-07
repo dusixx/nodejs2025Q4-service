d9def58aed3bd0ca0a99b31a277f77a5d2486e24
dusixx <113414935+dusixx@users.noreply.github.com>
2025-12-08 00:51:34 +0200

    fix: remove globals.d.ts

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  type ProcessEnv = {
-    PORT: string;
-    POSTGRES_PORT: string;
-    TEST_MODE: string;
-    POSTGRES_URL: string;
-    NODE_ENV: 'development' | 'production' | 'test';
-  };
-}
