d2d882fb9cbcf3ce316b4f95cb6e3bb9bad6c26b
dusixx <113414935+dusixx@users.noreply.github.com>
2025-12-14 09:51:56 +0200

    chore: update constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -5,7 +5,7 @@ export const DEF_CRYPT_SALT = 10;
 export const DEF_ACCESS_TOKEN_EXPIRE_TIME = '1h';
 export const DEF_REFRESH_TOKEN_EXPIRE_TIME = '24h';
 export const DEF_LOG_LEVEL = 4;
-export const DEF_LOG_MAX_SIZE_KB = 150;
+export const DEF_LOG_MAX_SIZE_KB = 50;
 
 export const ErrorMessage = {
   InvalidUUID: 'invalid UUID',
